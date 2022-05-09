eec0552ef4811a6e3aafaf7ddc6c16dde9879a69
Jesper Stemann Andersen <jsa@ihp.dk>
2022-05-09 19:18:47 +0200

    Added lock to ResourcePool - fixes multi-threading test

--- src/resource_pool.jl
+++ src/resource_pool.jl
@@ -2,6 +2,7 @@ abstract type AbstractResourcePool{T} end
 
 struct ResourcePool{T} <: AbstractResourcePool{T}
     pool::Queue{T}
+    lock::ReentrantLock
 end
 
 function ResourcePool{T}(resources::AbstractVector{T}) where T
@@ -9,7 +10,7 @@ function ResourcePool{T}(resources::AbstractVector{T}) where T
     for resource in resources
         enqueue!(pool, resource)
     end
-    return ResourcePool(pool)
+    return ResourcePool(pool, ReentrantLock())
 end
 
 function ResourcePool{T}(size::Int, create_resource::Function) where T
@@ -17,25 +18,29 @@ function ResourcePool{T}(size::Int, create_resource::Function) where T
     for i = 1:size
         enqueue!(pool, create_resource())
     end
-    return ResourcePool(pool)
+    return ResourcePool(pool, ReentrantLock())
 end
 
 Base.length(p::AbstractResourcePool) = length(p.pool)
 
 function Base.take!(p::AbstractResourcePool{T}) where T
-    if length(p) < 1
-        throw(ArgumentError("$p is empty!"))
-    end
     function dispose(r)
         @debug "Returning $(resource(r)) to $(p)"
-        enqueue!(p.pool, resource(r))
+        lock(p.lock) do
+            enqueue!(p.pool, resource(r))
+        end
     end
-    r = dequeue!(p.pool)
-    if T <: DenseArray
-        PooledArray(r, dispose)
-    elseif T <: AbstractArray
-        PooledAbstractArray(r, dispose)
-    else
-        PooledResource(r, dispose)
+    lock(p.lock) do
+        if length(p) < 1
+            throw(ArgumentError("$p is empty!"))
+        end
+        r = dequeue!(p.pool)
+        if T <: DenseArray
+            return PooledArray(r, dispose)
+        elseif T <: AbstractArray
+            return PooledAbstractArray(r, dispose)
+        else
+            return PooledResource(r, dispose)
+        end
     end
 end
